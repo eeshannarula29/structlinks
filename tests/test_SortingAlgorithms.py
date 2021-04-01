cc0bde6cab93b327563784b626a69a18c3e2bad7
Roman Zupancic <roman.k.zupancic@gmail.com>
2021-04-01 09:57:06 -0400

    Overhauled sorting algorithm test suite (now with inheritance of test cases)

--- tests/test_SortingAlgorithms.py
+++ tests/test_SortingAlgorithms.py
@@ -5,6 +5,8 @@ current working directory to PYTHONPATH, in order to 'see'
 the functions it is testing
 """
 
+from typing import Optional, Callable
+
 # Add the root directory to PYTHONPATH
 import sys
 sys.path.append('.')
@@ -13,232 +15,128 @@ from SortingAlgorithms import *
 
 
 ########################################
-# Quick sort (In Place)
+# Base tests for non-mutating sorts
 ########################################
-class TestMergeSort:
+class BaseNoMutatingSort:
+    """The base class for a suit of tests that work with in place sorting algorithms.
+
+    Inherit from this class and assign an algorithm to be used in the tests.
+    """
+    algorithm: Callable[[], None] = staticmethod(None)
+
     def test_unsortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
+        """Test algorithm on an unsorted list"""
         test_list = [9, 7, 5, 2, 4, 5, 3, 3, 2, 1, 10, 200]
 
-        actual = mergesort(test_list)
+        actual = self.algorithm(test_list)
         expected = sorted(test_list)
 
         assert actual == expected
-
 
     def test_sortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
+        """Test algorithm on a sorted list"""
         test_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-        actual = mergesort(test_list)
+        actual = self.algorithm(test_list)
         expected = sorted(test_list)
 
         assert actual == expected
-
 
     def test_reversedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
+        """Test algorithm on a reversed list"""
         test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-        actual = mergesort(test_list)
+        actual = self.algorithm(test_list)
         expected = sorted(test_list)
 
         assert actual == expected
 
-
     def test_key_reverse(self) -> None:
-        """Test mergesort on an unsorted list"""
+        """Test algorithm on a list, sort with a reversing key."""
         test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-        actual = mergesort(test_list, (lambda x: -x))
+        actual = self.algorithm(test_list, (lambda x: -x))
         expected = sorted(test_list, key=(lambda x: -x))
 
         assert actual == expected
 
 
 ########################################
-# Quick sort (In Place)
+# Base tests for mutating sorts
 ########################################
-class TestInPlaceQuickSort:
+class BaseInPlaceSort:
+    """The base class for a suit of tests that work with in place sorting algorithms.
+
+    Inherit from this class and assign an algorithm to be used in the tests.
+    """
+    algorithm: Callable[[], None] = staticmethod(None)
+
     def test_unsortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
+        """Test algorithm on an unsorted list"""
         test_list = [9, 7, 5, 2, 4, 5, 3, 3, 2, 1, 10, 200]
 
         expected = sorted(test_list.copy())
-        quicksort(test_list)
+        self.algorithm(test_list)
         actual = test_list.copy()
 
         assert actual == expected
 
-
     def test_sortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
+        """Test algorithm on an sorted list"""
         test_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
         expected = sorted(test_list.copy())
-        quicksort(test_list)
+        self.algorithm(test_list)
         actual = test_list.copy()
 
         assert actual == expected
 
-
     def test_reversedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
+        """Test algorithm on a reversed list"""
         test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
         expected = sorted(test_list.copy())
-        quicksort(test_list)
+        self.algorithm(test_list)
         actual = test_list.copy()
 
         assert actual == expected
 
-
     def test_key_reverse(self) -> None:
-        """Test mergesort on an unsorted list"""
+        """Test algorithm on a list, sort with a reversing key."""
         test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-        quicksort(test_list, (lambda x: -x))
+        self.algorithm(test_list, (lambda x: -x))
         actual = test_list.copy()
         expected = sorted(test_list.copy(), key=(lambda x: -x))
 
         assert actual == expected
 
 
-########################################
-# Quick sort (Out of Place)
-########################################
-class TestNoMutQuickSort:
-    def test_unsortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [9, 7, 5, 2, 4, 5, 3, 3, 2, 1, 10, 200]
-
-        actual = no_mut_quicksort(test_list)
-        expected = sorted(test_list)
-
-        assert actual == expected
+class TestNoMutatingMergesort(BaseNoMutatingSort):
+    """Test the non-mutating mergesort algorithm"""
+    algorithm: Optional[tuple[Callable[[list], None]]] = staticmethod(mergesort)
 
 
-    def test_sortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-        actual = no_mut_quicksort(test_list)
-        expected = sorted(test_list)
-
-        assert actual == expected
+# class TestInPlaceMergesort(BaseNoMutatingSort):
+#     """Test the non-mutating mergesort algorithm"""
+#     algorithm: Optional[tuple[Callable[[list], None]]] = staticmethod(in_place_mergesort)
 
 
-    def test_reversedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-
-        actual = no_mut_quicksort(test_list)
-        expected = sorted(test_list)
-
-        assert actual == expected
+class TestInPlaceQuicksort(BaseInPlaceSort):
+    """Test the mutating quicksort algorithm"""
+    algorithm: Optional[tuple[Callable[[list], None]]] = staticmethod(quicksort)
 
 
-    def test_key_reverse(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-
-        actual = no_mut_quicksort(test_list, (lambda x: -x))
-        expected = sorted(test_list, key=(lambda x: -x))
-
-        assert actual == expected
+class TestNoMutatingQuicksort(BaseNoMutatingSort):
+    """Test the non-mutating quicksort algorithm"""
+    algorithm: Optional[tuple[Callable[[list], None]]] = staticmethod(no_mut_quicksort)
 
 
-########################################
-# Selection Sort
-########################################
-class TestSelectionSort:
-    def test_unsortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [9, 7, 5, 2, 4, 5, 3, 3, 2, 1, 10, 200]
-
-        expected = sorted(test_list.copy())
-        selection_sort(test_list)
-        actual = test_list.copy()
-
-        assert actual == expected
+class TestInPlaceInsertionSort(BaseInPlaceSort):
+    """Test the mutating insertionsort algorithm"""
+    algorithm: Optional[tuple[Callable[[list], None]]] = staticmethod(insertion_sort)
 
 
-    def test_sortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-        expected = sorted(test_list.copy())
-        selection_sort(test_list)
-        actual = test_list.copy()
-
-        assert actual == expected
-
-
-    def test_reversedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-
-        expected = sorted(test_list.copy())
-        selection_sort(test_list)
-        actual = test_list.copy()
-
-        assert actual == expected
-
-
-    def test_key_reverse(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-
-        selection_sort(test_list, (lambda x: -x))
-        actual = test_list.copy()
-        expected = sorted(test_list.copy(), key=(lambda x: -x))
-
-        assert actual == expected
-
-
-########################################
-# Insertion Sort
-########################################
-class TestInsertionSort:
-    def test_unsortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [9, 7, 5, 2, 4, 5, 3, 3, 2, 1, 10, 200]
-
-        expected = sorted(test_list.copy())
-        insertion_sort(test_list)
-        actual = test_list.copy()
-
-        assert actual == expected
-
-
-    def test_sortedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-        expected = sorted(test_list.copy())
-        insertion_sort(test_list)
-        actual = test_list.copy()
-
-        assert actual == expected
-
-
-    def test_reversedlist(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-
-        expected = sorted(test_list.copy())
-        insertion_sort(test_list)
-        actual = test_list.copy()
-
-        assert actual == expected
-
-
-    def test_key_reverse(self) -> None:
-        """Test mergesort on an unsorted list"""
-        test_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-
-        insertion_sort(test_list, (lambda x: -x))
-        actual = test_list.copy()
-        expected = sorted(test_list.copy(), key=(lambda x: -x))
-
-        assert actual == expected
+class TestInPlaceSelectionSort(BaseInPlaceSort):
+    """Test the mutating selection sort algorithm"""
+    algorithm: Optional[tuple[Callable[[list], None]]] = staticmethod(selection_sort)
