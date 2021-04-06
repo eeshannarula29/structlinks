325aeab3cfe35524774e463760c937464c9c9150
eeshannarula29 <eeshannarula29@gmail.com>
2021-04-07 03:39:57 +0530

    added-basic-graph-searching

--- SearchingAlgorithms.py
+++ SearchingAlgorithms.py
@@ -7,10 +7,16 @@ Many of these sort are inspired by their implementations in the CSC111
 University of Toronto Course.
 """
 
-from typing import Any
+from typing import Any, Union, Optional
+
+from Graph import Graph
+from Queue import Queue
+from Stack import Stack
+from LinkedList import LinkedList
+from DoublyLinkedList import DoublyLinkedList
 
 
-def linear_search(lst: list, item: Any) -> bool:
+def linear_search(lst: Union[list, LinkedList, DoublyLinkedList], item: Any) -> bool:
     """Return a boolean representing whether or not the given item
     exists in the list.
     """
@@ -23,7 +29,7 @@ def linear_search(lst: list, item: Any) -> bool:
     return False
 
 
-def binary_search(lst: list, item: Any) -> bool:
+def binary_search(lst: Union[list, LinkedList, DoublyLinkedList], item: Any) -> bool:
     """Return a boolean representing whether or not the given item
     exists in the list.
 
@@ -49,3 +55,69 @@ def binary_search(lst: list, item: Any) -> bool:
 
     # If we reach this point, the item is not in the list
     return False
+
+
+def _edges_from_path(lst: list) -> list[tuple]:
+    """Return a list of edges form the given path"""
+    edges = []
+
+    for index in range(len(lst) - 1):
+        edges.append((lst[index], lst[index + 1]))
+
+    return edges
+
+
+def BreadthFirstSearch(g: Graph, origin: Any, target: Any) -> Optional[list]:
+    """Search for the smallest path between two nodes of a graph using Queues
+
+    source: https://www.geeksforgeeks.org/print-paths-given-source-destination-using-bfs/
+    """
+    if origin in g.vertices:
+        path = [origin]
+        queue = Queue()
+        queue.enqueue(path)
+
+        while queue:
+
+            path = queue.dequeue()
+            last = path[len(path) - 1]
+
+            if last == target:
+                return _edges_from_path(path)
+
+            for neighbour in g[last].get_neighbours():
+                if neighbour not in path:
+                    new_path = path.copy()
+                    new_path.append(neighbour)
+                    queue.enqueue(new_path)
+
+        return None
+    else:
+        raise ValueError('the vertices do not exist in the graph')
+
+
+def DepthFirstSearch(g: Graph, origin: Any, target: Any) -> Optional[list]:
+    """Search for the shortest path between two nodes of a graph using Stacks
+    """
+    if origin in g.vertices:
+        path = [origin]
+        stack = Stack()
+        stack.push(path)
+
+        while stack:
+
+            path = stack.pop()
+            last = path[len(path) - 1]
+
+            if last == target:
+                return _edges_from_path(path)
+
+            for neighbour in g[last].get_neighbours():
+                if neighbour not in path:
+                    new_path = path.copy()
+                    new_path.append(neighbour)
+                    stack.push(new_path)
+
+        return None
+    else:
+        raise ValueError('the vertices do not exist in the graph')
