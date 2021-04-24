91564a5aaf9ba0b2e688d3d4f8284251aee66400
eeshannarula29 <eeshannarula29@gmail.com>
2021-04-24 22:47:44 +0530

    changed init and added mapping

--- Matrix.py
+++ Matrix.py
@@ -1,8 +1,7 @@
 """This file contains the Matrix object."""
 
 from __future__ import annotations
-from typing import Union
-import pprint
+from typing import Union, Callable
 import copy
 
 
@@ -16,38 +15,85 @@ class Matrix:
     # Private Instance Attributes:
     #       - _matrix: A 2-D list of integers or floats
     _matrix: list[list[Union[int, float]]]
-    row_count: int
-    col_count: int
+    _row_count: int
+    _col_count: int
 
-    def __init__(self, m: int, n: int) -> None:
-        """Initialize a new m x n matrix with all ones, where m is the number of rows
-        and n is the number of columns
-
-        Preconditions:
-            - m >= 0 and n >= 0
+    def __init__(self, array: list[list[Union[int, float]]]) -> None:
+        """Initialize a new matrix
         """
-        if m == 0 or n == 0:
-            self._matrix = []
-            self.row_count = 0
-            self.col_count = 0
-        else:
-            self._matrix = []
-            for _ in range(m):
-                self._matrix.append([1 for _ in range(n)])
-            
-            self.row_count = m
-            self.col_count = n
+        self._row_count = len(array)
+        self._col_count = len(array[0]) if self._row_count > 0 else 0
+
+        self.shape = (self._row_count, self._col_count)
+
+        self._matrix = array
+
+    def __getitem__(self, index: int) -> list:
+        """ Return the row at index <index>"""
+        if self._row_count <= index:
+            raise IndexError
+        return self._matrix[index]
+
+    def __str__(self) -> str:
+        return '[' + '\n '.join([str(row) for row in self._matrix]) + ']'
+
+    def __repr__(self) -> str:
+        return 'Matrix([' + '\n        '.join([str(row) for row in self._matrix]) + '])'
+
+    def copy(self) -> Matrix:
+        return Matrix(copy.deepcopy(self._matrix))
+
+    def to_list(self) -> list[list[Union[int, float]]]:
+        return copy.deepcopy(self._matrix)
+
+    @property
+    def is_square(self) -> bool:
+        """Return whether the current matrix is square"""
+        return self._row_count == self._col_count
+
+    @property
+    def is_invertible(self) -> bool:
+        """Return whether the current matrix is invertible"""
+        return self.determinant() != 0
+
+    @staticmethod
+    def zeros(shape: tuple[int, int]) -> Matrix:
+        """Return a zero matrix of shape <shape>"""
+        return Matrix([[0] * shape[1] for _ in range(shape[0])])
+
+    @staticmethod
+    def ones(shape: tuple[int, int]) -> Matrix:
+        """Return a matrix with all elements one of shape <shape>"""
+        return Matrix([[1] * shape[1] for _ in range(shape[0])])
+
+    @staticmethod
+    def identity(order: int) -> Matrix:
+        """Return a identity matrix of shape order X order"""
+        return Matrix([[1 if i == j else 0 for j in range(order) for i in range(order)]])
+
+    def map(self, func: Callable) -> Matrix:
+        """Return a new matrix mapped to function <func>"""
+        copy_mat = self.copy()
+
+        for i in range(self._row_count):
+            for j in range(self._col_count):
+                copy_mat[i][j] = func(copy_mat[i][j])
+
+        return copy_mat
+
+    def add_scalar(self, k: Union[int, float]) -> Matrix:
+        """Add the current matrix by the scalar k
+        to produce a new matrix. This operation does not mutate
+        the original matrix
+        """
+        return self.map(lambda x: x + k)
 
     def multiply_scalar(self, k: Union[int, float]) -> Matrix:
         """Multiply the current matrix by the scalar k
         to produce a new matrix. This operation does not mutate
         the original matrix
         """
-        new_matrix = Matrix(self.row_count, self.col_count)
-        for i in range(self.row_count):
-            for j in range(self.col_count):
-                new_matrix._matrix[i][j] *= k
-        return new_matrix
+        return self.map(lambda x: x * k)
     
     def add_matrix(self, b: Matrix) -> Matrix:
         """Add the current matrix and matrix b to produce a
@@ -57,20 +103,12 @@ class Matrix:
             - self.row_count == b.row_count
             - self.col_count == b.col_count
         """
-        sum_matrix = Matrix(self.row_count, self.col_count)
-        for i in range(self.row_count):
-            for j in range(self.col_count):
+        sum_matrix = Matrix.zeros((self._row_count, self._col_count))
+        for i in range(self._row_count):
+            for j in range(self._col_count):
                 # Add the corresponding entries for both matrices
-                sum_matrix._matrix[i][j] = self._matrix[i][j] + b._matrix[i][j]
+                sum_matrix[i][j] = self[i][j] + b[i][j]
         return sum_matrix
-    
-    def is_square(self) -> bool:
-        """Return whether the current matrix is square"""
-        return self.row_count == self.col_count
-    
-    def print_matrix(self) -> None:
-        """Print the current matrix to the console"""
-        pprint.pprint(self._matrix)
     
     def minor(self, row: int, col: int) -> Matrix:
         """Return the minor of the matrix with the given row and
@@ -80,18 +118,15 @@ class Matrix:
             - 0 <= row < self.row_count
             - 0 <= col < self.col_count 
         """
-        new_matrix = Matrix(self.row_count - 1, self.col_count - 1)
-        for i in range(self.row_count):
-            for j in range(self.col_count):
-                if i < row and j < col:
-                    new_matrix._matrix[i][j] = self._matrix[i][j]
-                elif i < row and j > col:
-                    new_matrix._matrix[i][j - 1] = self._matrix[i][j]
-                elif i > row and j < col:
-                    new_matrix._matrix[i - 1][j] = self._matrix[i][j]
-                elif i > row and j > col:
-                    new_matrix._matrix[i - 1][j - 1] = self._matrix[i][j]
-        return new_matrix
+        # create a new array
+        copy_mat = self.to_list()
+        # remove the row
+        copy_mat.pop(row)
+        # remove the col from all the other rows
+        for row in copy_mat:
+            row.pop(col)
+        # return the new matrix
+        return Matrix(copy_mat)
 
     def determinant(self) -> Union[float, int]:
         """Return the determinant of the current matrix
@@ -99,40 +134,12 @@ class Matrix:
         Preconditions:
             - self.row_count == self.col_count
         """
-        if self.row_count == 0 and self.col_count == 0:
+        if self._row_count == 0 and self._col_count == 0:
             return 1
-        elif self.row_count == 1 and self.col_count == 1:
-            return self._matrix[0][0]
+        elif self._row_count == 1 and self._col_count == 1:
+            return self[0][0]
         else:
             determinant = 0
-            for j in range(self.col_count):
-                determinant += self._matrix[0][j] * ((-1) ** j) * self.minor(0, j).determinant()
+            for j in range(self._col_count):
+                determinant += self[0][j] * ((-1) ** j) * self.minor(0, j).determinant()
             return determinant
-    
-    def is_invertible(self) -> bool:
-        """Return whether the current matrix is invertible"""
-        return self.determinant() != 0
-    
-    def get_matrix(self) -> list[list[Union[int, float]]]:
-        """Return a copy of the list of lists representing the matrix"""
-        return copy.deepcopy(self._matrix)
-    
-    def set_matrix(self, input: list[list[Union[int, float]]]) -> None:
-        """Set the current matrix to a copy of the given input
-        
-        Preconditions:
-            - all(len(list_1) == len(list_2) for list_1 in input for list_2 in input)
-        """
-        self._matrix = copy.deepcopy(input)
-        self.row_count = len(input)
-        if len(input) == 0:
-            self.col_count = 0
-        else:
-            self.col_count = len(input[0])
-    
-    def __getitem__(self, pos: tuple[int, int]) -> Union[int, float]:
-        """Return the entry at the i-th row and j-th column"""
-        i, j = pos
-        # Subtract 1 since indexing starts at 0
-        return self._matrix[i - 1][j - 1]
-
