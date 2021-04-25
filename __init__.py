ce771b38b01ac622e9b0f38536866d98cb69a7b2
eeshannarula29 <69961210+eeshannarula29@users.noreply.github.com>
2021-04-25 19:06:47 +0530

    Update __init__.py
    add matrix lib

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from structlinks.LinkedList import *
 from structlinks.DoublyLinkedList import *
 from structlinks.Graph import *
+from structlinks.Matrix import *
 from structlinks.Stack import *
 from structlinks.Queue import *
 import structlinks.SortingAlgorithms as sorting_algorithms
