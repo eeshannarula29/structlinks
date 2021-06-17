d444bd27e7b2a01fa93d0a63dab485a5a8990b0d
eeshannarula29 <eeshannarula29@gmail.com>
2021-06-17 17:47:37 +0530

    added heaps

--- DataStructures/__init__.py
+++ DataStructures/__init__.py
@@ -4,3 +4,4 @@ from structlinks.DataStructures.Graph.Graph import Graph
 from structlinks.DataStructures.LinkedList.LinkedList import LinkedList
 from structlinks.DataStructures.Matrix.Matrix import Matrix
 from structlinks.DataStructures.Queue.Queue import Queue
+from structlinks.DataStructures.Heap.Heap import Heap, MaxHeap, MinHeap, PriorityFunction
